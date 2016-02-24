04c5f7b2932011f64a9caefdcf084925d964ae4d
treyboyd98 <treyboyd98@gmail.com>
2016-02-24 09:40:46 -0600

    Added ListView of all songs on the device, internal and external storage

--- app/src/main/java/dirk/net/audioplayer/MusicListFragment.java
+++ app/src/main/java/dirk/net/audioplayer/MusicListFragment.java
@@ -1,10 +1,22 @@
 package dirk.net.audioplayer;
 
+import android.content.ContentResolver;
+import android.content.Context;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.LinearLayout;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 /**
  * Created by PLTW on 2/23/2016.
@@ -15,6 +27,10 @@ public class MusicListFragment extends Fragment {
      * fragment.
      */
     private static final String ARG_SECTION_NUMBER = "section_number";
+
+    private Context context;
+    private ListView songView;
+    private ArrayList songList;
 
     /**
      * Returns a new instance of this fragment for the given section
@@ -35,6 +51,97 @@ public class MusicListFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_music_list, container, false);
+        context = getActivity().getApplicationContext();
+
+        //retrieve list view
+        songView = (ListView)rootView.findViewById(R.id.song_listview);
+        //instantiate list
+        songList = new ArrayList<Song>();
+        //get songs from device
+        getSongList();
+        //sort alphabetically by title
+        Collections.sort(songList, new Comparator<Song>() {
+            public int compare(Song a, Song b) {
+                return a.getTitle().compareTo(b.getTitle());
+            }
+        });
+        //create and set adapter
+        SongAdapter songAdt = new SongAdapter(context, songList);
+        songView.setAdapter(songAdt);
+        //
+
         return rootView;
+    }
+
+    //method to retrieve song info from device
+    public void getSongList(){
+        //query external audio
+        ContentResolver musicResolver = getActivity().getContentResolver();
+        Uri musicUri = android.provider.MediaStore.Audio.Media.EXTERNAL_CONTENT_URI;
+        Cursor musicCursor = musicResolver.query(musicUri, null, null, null, null);
+        //iterate over results if valid
+        if(musicCursor!=null && musicCursor.moveToFirst()){
+            //get columns
+            int titleColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.TITLE);
+            int idColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media._ID);
+            int artistColumn = musicCursor.getColumnIndex
+                    (android.provider.MediaStore.Audio.Media.ARTIST);
+            //add songs to list
+            do {
+                long thisId = musicCursor.getLong(idColumn);
+                String thisTitle = musicCursor.getString(titleColumn);
+                String thisArtist = musicCursor.getString(artistColumn);
+                songList.add(new Song(thisId, thisTitle, thisArtist));
+            }
+            while (musicCursor.moveToNext());
+        }
+    }
+
+    public class SongAdapter extends BaseAdapter {
+
+        private ArrayList<Song> songs;
+        private LayoutInflater songInf;
+
+        public SongAdapter(Context c, ArrayList<Song> theSongs) {
+            songs = theSongs;
+            songInf = LayoutInflater.from(c);
+        }
+
+        @Override
+        public int getCount() {
+            return songs.size();
+        }
+
+        @Override
+        public Object getItem(int arg0) {
+            // TODO Auto-generated method stub
+            return null;
+        }
+
+        @Override
+        public long getItemId(int arg0) {
+            // TODO Auto-generated method stub
+            return 0;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            //map to song layout
+            LinearLayout songLay = (LinearLayout) songInf.inflate
+                    (R.layout.song, parent, false);
+            //get title and artist views
+            TextView songView = (TextView) songLay.findViewById(R.id.song_title);
+            TextView artistView = (TextView) songLay.findViewById(R.id.song_artist);
+            //get song using position
+            Song currSong = songs.get(position);
+            //get title and artist strings
+            songView.setText(currSong.getTitle());
+            artistView.setText(currSong.getArtist());
+            //set position as tag
+            songLay.setTag(position);
+            return songLay;
+        }
     }
 }
---
+++ app/src/main/java/dirk/net/audioplayer/Song.java
@@ -0,0 +1,21 @@
+package dirk.net.audioplayer;
+
+/**
+ * Created by Melvin Boyd on 2/24/2016.
+ */
+public class Song {
+
+    private long id;
+    private String title;
+    private String artist;
+
+    public Song(long songID, String songTitle, String songArtist) {
+        id=songID;
+        title=songTitle;
+        artist=songArtist;
+    }
+
+    public long getID(){return id;}
+    public String getTitle(){return title;}
+    public String getArtist(){return artist;}
+}
